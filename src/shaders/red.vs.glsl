c1365f6594612a51e25b35c196a92b3455ea0792
Akumaxblend <83503168+Akumaxblend@users.noreply.github.com>
2024-04-01 18:38:13 +0200

    Shader updated to fit 3D requirements

--- src/shaders/red.vs.glsl
+++ src/shaders/red.vs.glsl
@@ -1,14 +1,22 @@
 #version 330 core
 
-layout(location = 0) in vec2 aVertexPosition;
+layout(location = 0) in vec3 aVertexPosition;
 layout(location = 1) in vec3 aVertexNormal;
 
 out vec3 vFragNormal;
+out vec3 vFragPosition;
+//out vec2 vFragUV;
 
-uniform mat3 u_translate_boid_matrix;
+uniform mat4 uMVPMatrix;
+uniform mat4 uMVMatrix;
+uniform mat4 uNormalMatrix;
 
 void main() {
-    vec2 vVertexPosition = vec2(u_translate_boid_matrix * vec3(aVertexPosition, 1)).xy;
-    gl_Position = vec4(vVertexPosition, 0, 1);
-    vFragNormal = aVertexNormal;
+    vec4 vertexPosition = vec4(aVertexPosition, 1);
+    vec4 vertexNormal = vec4(aVertexNormal, 0);
+
+    vFragPosition = vec3 (uMVMatrix * vertexPosition);
+    vFragNormal = vec3 (uNormalMatrix * vertexNormal);
+    //vFragUV = aVertexUV;
+    gl_Position = vec4(uMVPMatrix * vertexPosition);
 };
