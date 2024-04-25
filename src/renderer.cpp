b0379461cf4de2538df409524ebaa3c185ae1c13
Alex-665 <alexmiralles65@gmail.com>
2024-04-25 10:03:34 +0200

    refactoring of attribPointer, etc

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,5 +1,50 @@
 #include "renderer.hpp"
 
+void Renderer::setupBufferData() 
+{
+    m_vbo.bind(0);
+    glBufferData(GL_ARRAY_BUFFER, m_object.vertices.size() * sizeof(vertex), m_object.vertices.data(), GL_STATIC_DRAW);
+    m_vbo.unbind();
+}
+
+void Renderer::setupAttribArray(const bool instanced)
+{
+    m_vao.bind();
+    const GLuint VERTEX_ATTR_POSITION = 0;
+    const GLuint VERTEX_ATTR_NORMAL = 1;
+    const GLuint VERTEX_ATTR_TEXTURE = 2;
+    glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+    glEnableVertexAttribArray(VERTEX_ATTR_NORMAL);
+    glEnableVertexAttribArray(VERTEX_ATTR_TEXTURE);
+    m_vbo.bind(0);
+    glVertexAttribPointer(VERTEX_ATTR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (const GLvoid*)offsetof(vertex, position));
+    glVertexAttribPointer(VERTEX_ATTR_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (const GLvoid*)offsetof(vertex, normal));
+    glVertexAttribPointer(VERTEX_ATTR_TEXTURE, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), (const GLvoid*)offsetof(vertex, uv));
+    m_vbo.unbind();
+
+    if (instanced) {
+        //to enable the four vec4 of the instance_matrix
+        glEnableVertexAttribArray(3);
+        glEnableVertexAttribArray(4);
+        glEnableVertexAttribArray(5);
+        glEnableVertexAttribArray(6);
+
+        //way for the m_vao to read the matrix of transformation
+        m_vbo.bind(1);
+        glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (const GLvoid*)(0 * sizeof(glm::vec4)));
+        glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (const GLvoid*)(1 * sizeof(glm::vec4)));
+        glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (const GLvoid*)(2 * sizeof(glm::vec4)));
+        glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (const GLvoid*)(3 * sizeof(glm::vec4)));
+        glVertexAttribDivisor(3, 1);
+        glVertexAttribDivisor(4, 1);
+        glVertexAttribDivisor(5, 1);
+        glVertexAttribDivisor(6, 1);
+        //
+        m_vbo.unbind();
+    }
+    m_vao.unbind();
+}
+
 void Renderer::drawClassic()
 {
     m_vao.bind();
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -3,14 +3,20 @@
 #include "vbo.hpp"
 #include "loader.h"
 
-//attention si on veut passer un shader dans les attributs ça fait quelque chose de bizarre (mais y en n'a pas besoin pour l'instant donc je mets juste ça là au cas où)
 class Renderer {
     private:
         Vao m_vao;
+        Vbo m_vbo;
         Object3D m_object;
+        //p6::Shader m_shader;
+        void setupBufferData();
+        void setupAttribArray(const bool instanced);
     public:
-        //note : passer le vao en référence parce que sinon c'est fait par copie et forcément opengl aime pas trop bcp ça
-        Renderer(Vao &vao, Object3D &object): m_vao(vao), m_object(object) {};
+        Renderer(Vao &vao, Vbo &vbo, Object3D &object, const bool instanced): m_vao(vao), m_vbo(vbo), m_object(object)
+    {
+        setupBufferData();
+        setupAttribArray(instanced);
+    };
         void drawClassic();
         void drawInstanced(int number);
 };
